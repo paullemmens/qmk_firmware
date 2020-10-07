47d88b88f4eacc3e75613ffc6544aacd269a5053
Paul Lemmens <paul.lemmens@gmail.com>
2020-10-07 22:35:37 +0200

    Add key combo for moving windows in Windows 10

--- keyboards/kyria/keymaps/paullemmens/keymap.c
+++ keyboards/kyria/keymaps/paullemmens/keymap.c
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   `    |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |  ESC   | Lgui | Lalt | Lctrl| raise|Lshift|                              |Rshift| raise| Rctrl| AltGr| Rgui |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |  C   |   V  |   B  |      |      |  | Del  | RAlt |   N  |   M  | ,  < | . >  | /  ? | RShift |
- * |        |      |      |      | lower|      |      |      |  | Raise| BS   |      | lower|      |      |      |        |
+ * | LShift |   Z  |   X  |  C   |   V  |   B  |      |S-Gui-|  | Del  | RAlt |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * |        |      |      |      | lower|      |      | lower|  | Raise| BS   |      | lower|      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | LGUI | raise| Space| LCtrl|      |  | RCtrl| Enter| Space| AltGr| RGui |
  *                        |      |      | Lower|      |      |  |      |RSHift|      |      |      |
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_TAB,  KC_Q,         KC_W,         KC_E,          LT(_ADJUST, KC_R), KC_T,                                                                                            KC_Y,         LT(_ADJUST, KC_U),   KC_I,         KC_O,         KC_P,            KC_BSPC,
       KC_GRV,  LGUI_T(KC_A), LALT_T(KC_S), LCTRL_T(KC_D), LT(_RAISE, KC_F),  LSFT_T(KC_G),                                                                                    RSFT_T(KC_H), LT(_RAISE, KC_J),    RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-      KC_LSFT, KC_Z,         KC_X,         KC_C,          LT(_LOWER, KC_V),  KC_B,               _______,  _______,                 LT(_RAISE, KC_DEL), MT(KC_RALT, KC_BSPC), KC_N,         LT(_LOWER, KC_M),    KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
-                                           KC_LGUI,       TT(_RAISE),        LT(_LOWER, KC_SPC), KC_LCTRL, LT(_RAISE, KC_ESC),      KC_RCTRL,           MT(KC_RSFT, KC_ENT),  KC_SPACE,     KC_RALT,             KC_RGUI
+      KC_LSFT, KC_Z,         KC_X,         KC_C,          LT(_LOWER, KC_V),  KC_B,               _______,  SGUI_LWR,     LT(_RAISE, KC_DEL), MT(KC_RALT, KC_BSPC), KC_N,         LT(_LOWER, KC_M),    KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
+                                           KC_LGUI,       TT(_RAISE),        LT(_LOWER, KC_SPC), KC_LCTRL, GUI_LWR,      KC_RCTRL,           MT(KC_RSFT, KC_ENT),  KC_SPACE,     KC_RALT,             KC_RGUI
     ),
 /*
  * Lower Layer: function keys, arrows, and symbols

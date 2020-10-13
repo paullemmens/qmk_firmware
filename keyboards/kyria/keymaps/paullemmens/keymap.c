26bdb1fc730999f7f5389c2ef10bf2702966c294
Paul Lemmens <paul.lemmens@gmail.com>
2020-10-13 13:56:21 +0200

    Add backslash and change keys' positions

--- keyboards/kyria/keymaps/paullemmens/keymap.c
+++ keyboards/kyria/keymaps/paullemmens/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |   1    |  2   |  3   |  4   |  5   |  6   |                              |  7   |  8   |  9   |  0   |  _   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  #   |  $   |  (   |  )   |  `   |                              | Left | Down |  Up  | Right|  \ | |  = +   |
+ * |        |  #   |  $   |  (   |  )   |  `   |                              | Left | Down |  Up  | Right|   |  |  = +   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |  %   |  ^   |  {   |  }   |  ~   |  [   |   ]  |  |      |      | Home | PgDn | PgUp |      | End  |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_LOWER] = LAYOUT(
       KC_1,        KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                                        KC_7,    KC_8,    KC_9,    KC_0,    S(KC_MINS), _______,
-      TO(_QWERTY), KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLSH,    KC_EQL,
+      TO(_QWERTY), KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, S(KC_BSLS), KC_EQL,
       _______,     KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_TILD, KC_LBRC, KC_RBRC, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, _______, KC_END,     _______,
                                      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Raise Layer: F-keys, Numpad
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  |                              | / ?  | 7 &  | 8 *  | 9 (  | - _  |        |
+ * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  |                              | / ?  | 7 &  | 8 *  | 9 (  | - _  |  \ |   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        |  F7  |  F8  |  F9  |  F10 |  F11 |                              |  €   | 4 $  | 5 %  | 6 ^  |  *   |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_RAISE] = LAYOUT(
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                       KC_SLSH,    KC_7,    KC_8, KC_9, KC_MINS, _______,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                       KC_SLSH,    KC_7,    KC_8, KC_9, KC_MINS, KC_BSLS,
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                                      ALGR(KC_5), KC_4,    KC_5, KC_6, KC_PDOT, _______,
       _______, KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, KC_0,       KC_1,    KC_2, KC_3, KC_PPLS, _______,
                                  _______, _______, _______, _______, _______, _______, KC_0,    KC_P0,      KC_PDOT, _______
@@ -91,21 +91,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Adjust Layer: Symbols, volume, locks, RGB
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |    !   |  @   |  #   |  $   |  %   |  ^   |                              |   &  |   *  |   (  |   )  |  _   | Insert |
+ * |    !   |  @   |  #   |  $   |  %   |  ^   |                              |   &  |   *  |   (  |   )  |  _   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  CAPS  | SAI  | HUI  | VAI  | Eff+ | MOD  |                              | BrDn | BrUp | Mute | VolDn| VolUp| Scrllck|
+ * |  CAPS  | SAI  | HUI  | VAI  | Eff+ | MOD  |                              | BrDn | BrUp | Mute | VolDn| VolUp|        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |  TOG   | SAD  | HUD  | VAD  | Eff- | RMOD | RGB P| RGB B|  | RGB R|RGB SW|RGB SN| RGB K| RGB X| RGB G| RGB T|  NumLk |
+ * |  TOG   | SAD  | HUD  | VAD  | Eff- | RMOD | RGB P| RGB B|  | RGB R|RGB SW|RGB SN| RGB K| RGB X| RGB G| RGB T|        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
+ *                        |      |      |      | NumLk|Scrllk|  |Insert|      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_ADJUST] = LAYOUT(
-      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                                       KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_INS,
-      KC_CAPS, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,                                       KC_BRID,  KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, KC_SLCK,
-      RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_SPD, RGB_RMOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, KC_NLCK,
-                                 _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______
+      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                                       KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, _______,
+      KC_CAPS, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,                                       KC_BRID,  KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+      RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_SPD, RGB_RMOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______,
+                                 _______, _______, _______,  KC_NLCK, KC_SLCK, KC_INS,  _______, _______, _______, _______
     ),
 // /*
 //  * Layer template

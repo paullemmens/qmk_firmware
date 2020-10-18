9d51fb3dee1be299743248f07ae81faa3f151830
Paul Lemmens <paul.lemmens@gmail.com>
2020-10-18 17:08:10 +0200

    Enable tap dance facility

--- keyboards/kyria/keymaps/paullemmens/rules.mk
+++ keyboards/kyria/keymaps/paullemmens/rules.mk
@@ -5,6 +5,7 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = no        # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
 
 # Bootloader selection
 #   Teensy       halfkay

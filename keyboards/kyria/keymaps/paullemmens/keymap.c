d75b4106e223b2f95b32985e149ea8e13e49e360
Paul Lemmens <paul.lemmens@gmail.com>
2020-10-24 23:07:29 +0200

    Add tap dance for 6/7 and ^/&
    Add tap dance for the circumflex and ampersand

--- keyboards/kyria/keymaps/paullemmens/keymap.c
+++ keyboards/kyria/keymaps/paullemmens/keymap.c
@@ -18,13 +18,17 @@
 // Tap dance stuff
 enum {
     TD_5_6,
-    TD_PERC_CIRC
+    TD_6_7,
+    TD_PERC_CIRC,
+    TD_CIRC_AMPR
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_5_6] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_6),
+    [TD_6_7] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_7),
     [TD_PERC_CIRC] = ACTION_TAP_DANCE_DOUBLE(KC_PERC, KC_CIRC)
+    [TD_CIRC_AMPR] = ACTION_TAP_DANCE_DOUBLE(KC_CIRC, KC_AMPR)
 };
 
 // Personal key definitions:
@@ -86,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Raise Layer: numbers, arrows, and some symbols
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |  1   |  2   |  3   |  4   |  5/6 |                              |  7   |   8  |   9  |  0   |  -   |        |
+ * |        |  1   |  2   |  3   |  4   |  5/6 |                              |  6/7 |   8  |   9  |  0   |  -   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        |  #   |  $   |  (   |  )   |  `   |                              |  4   |   5  |   6  |  +   |  *   |   +    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
@@ -97,10 +101,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_RAISE] = LAYOUT(
-      _______,     KC_1,    KC_2,    KC_3,    KC_4,    TD(TD_5_6),                                  KC_7, KC_8,    KC_9, KC_0,    KC_PMNS, _______,
-      TO(_QWERTY), KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                      KC_4, KC_5,    KC_6, KC_PPLS, KC_PAST, KC_PPLS,
-      _______,     KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_TILD, KC_LBRC, KC_RBRC, _______, _______, KC_1, KC_2,    KC_3, _______, KC_PSLS, _______,
-                                     _______, _______, _______, _______, _______, _______, KC_0,    KC_0, KC_PDOT, _______
+      _______,     KC_1,    KC_2,    KC_3,    KC_4,    TD(TD_5_6),                                  TD(TD_6_7), KC_8,    KC_9, KC_0,    KC_PMNS, _______,
+      TO(_QWERTY), KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                      KC_4,       KC_5,    KC_6, KC_PPLS, KC_PAST, KC_PPLS,
+      _______,     KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_TILD, KC_LBRC, KC_RBRC, _______, _______, KC_1,       KC_2,    KC_3, _______, KC_PSLS, _______,
+                                     _______, _______, _______, _______, _______, _______, KC_0,    KC_0,       KC_PDOT, _______
     ),
 /*
  * Adjust Layer: Symbols, volume, locks, RGB
@@ -117,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_ADJUST] = LAYOUT(
-      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  TD(TD_PERC_CIRC),                              KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, S(KC_EQL),
-      KC_CAPS, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,                                       KC_BRID,  KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, _______,
-      RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_SPD, RGB_RMOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______,
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  TD(TD_PERC_CIRC),                              TD(TD_CIRC_AMPR),  KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, S(KC_EQL),
+      KC_CAPS, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,                                       KC_BRID,           KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+      RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_SPD, RGB_RMOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN,          RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______,
                                  _______, _______, _______,  KC_NLCK, KC_SLCK, KC_INS,  _______, _______, _______, _______
     ),
 // /*

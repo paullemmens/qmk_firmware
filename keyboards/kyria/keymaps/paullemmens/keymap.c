00a1cb141bc26bd262c8bb335b57d55a3116b9b6
Paul Lemmens <paul.lemmens@gmail.com>
2020-10-27 15:22:23 +0100

    Swap e diaresis and e acute

--- keyboards/kyria/keymaps/paullemmens/keymap.c
+++ keyboards/kyria/keymaps/paullemmens/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
       _______,     KC_F1,   KC_F2,   KC_F3,   KC_F4,      KC_F5,                                                KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MINS,    KC_BSLS,
       TO(_QWERTY), _______, _______, KC_F11,  KC_F12,     KC_F6,                                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, S(KC_BSLS), KC_EQL,
-      _______,     _______, _______, _______, ALGR(KC_W), ALGR(KC_E), ALGR(KC_I), ALGR(KC_5), _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,    _______,
+      _______,     _______, _______, _______, ALGR(KC_E), ALGR(KC_W), ALGR(KC_I), ALGR(KC_5), _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,    _______,
                                      _______, _______,    _______,    _______,    _______,    _______, _______, _______, _______, _______
     ),
 /*

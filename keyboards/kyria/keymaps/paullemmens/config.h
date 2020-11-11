5623c958c1d959ed025cf9f84b4a91c2b7e123f6
Paul Lemmens <paul.lemmens@gmail.com>
2020-11-11 21:09:12 +0100

    Clean up config.h

--- keyboards/kyria/keymaps/paullemmens/config.h
+++ keyboards/kyria/keymaps/paullemmens/config.h
@@ -30,6 +30,15 @@
   /* #define RGBLIGHT_SPLIT */
 #endif
 
+// From j-inc kyria keyboard.
+/* #ifdef RGBLIGHT_ENABLE */
+/*     #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves. */
+/*     //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression. */
+/*     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
+/*     //#define RGBLIGHT_EFFECT_STATIC_GRADIENT */
+/*     #define RGBLIGHT_SLEEP */
+/* #endif */
+
 // The Leader key allows to flexibly assign macros to key sequences.
 /* #define LEADER_PER_KEY_TIMING */
 /* #define LEADER_TIMEOUT 350 */
@@ -49,19 +58,8 @@
 // Encoder settings.
 // Allows media codes to properly register in macros and rotary encoder code
 #define TAP_CODE_DELAY 10
-
 #define ENCODER_DIRECTION_FLIP
 // EC11K encoders have a different resolution than other EC11 encoders.
 // When using the default resolution of 4, if you notice your encoder skipping
 // every other tick, lower the resolution to 2.
-/* #define ENCODER_RESOLUTION 2 */
 #define ENCODER_RESOLUTION 2
-
-// From j-inc kyria keyboard.
-/* #ifdef RGBLIGHT_ENABLE */
-/*     #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves. */
-/*     //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression. */
-/*     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
-/*     //#define RGBLIGHT_EFFECT_STATIC_GRADIENT */
-/*     #define RGBLIGHT_SLEEP */
-/* #endif */

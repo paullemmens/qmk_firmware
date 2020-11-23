2e35f9439941265b4556f0f197870ac1d1d6af46
Paul Lemmens <paul.lemmens@gmail.com>
2020-11-23 20:47:18 +0100

    Set RGB led map based on splitkb faq

--- keyboards/kyria/keymaps/paullemmens/config.h
+++ keyboards/kyria/keymaps/paullemmens/config.h
@@ -33,16 +33,8 @@
   #define RGBLIGHT_LIMIT_VAL 150
   #define RGBLIGHT_SLEEP
   #define RGBLIGHT_SPLIT
+  #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13}
 #endif
-
-// From j-inc kyria keyboard.
-/* #ifdef RGBLIGHT_ENABLE */
-/*     #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves. */
-/*     //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression. */
-/*     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
-/*     //#define RGBLIGHT_EFFECT_STATIC_GRADIENT */
-/*     #define RGBLIGHT_SLEEP */
-/* #endif */
 
 // For home row mods
 #define TAPPING_TERM 250

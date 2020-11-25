55d3c0724037dde4b97fea76879985c9b707ec75
Paul Lemmens <paul.lemmens@gmail.com>
2020-11-25 20:42:07 +0100

    Store other led orientations

--- keyboards/kyria/keymaps/paullemmens/config.h
+++ keyboards/kyria/keymaps/paullemmens/config.h
@@ -33,7 +33,8 @@
   #define RGBLIGHT_LIMIT_VAL 150
   #define RGBLIGHT_SLEEP
   #define RGBLIGHT_SPLIT
-  #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13}
+  #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves.
+  //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression.
 #endif
 
 // For home row mods

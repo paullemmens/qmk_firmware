26e2f8e65aa2facf04f06f832227772f70cfdd40
Paul Lemmens <paul.lemmens@gmail.com>
2020-12-16 20:56:23 +0100

    Move mute to left encoder

--- keyboards/kyria/keymaps/paullemmens/keymap.c
+++ keyboards/kyria/keymaps/paullemmens/keymap.c
@@ -134,19 +134,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |   VLK  |  !   |  @   |  #   |  $   |  %   |                              |  ^   |   &  |   *  |   (  |  _   |   _    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  CAPS  | SAI  | HUI  | VAI  | Eff+ | MOD  |                              | BrDn | BrUp | Mute |      |      |        |
+ * |  CAPS  | SAI  | HUI  | VAI  | Eff+ | MOD  |                              | BrDn | BrUp |      |      |      |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |  RTOG  |      |      |      |      |      | RGB P| RGB B|  | RGB R|RGB SW|RGB SN| RGB K| RGB X| RGB G| RGB T|        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      | NumLk|Scrllk|  |Insert|      |      |      |      |
+ *                        | Mute |      |      | NumLk|Scrllk|  |Insert|      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_ADJUST] = LAYOUT(
       VLK_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                      KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_UNDS, KC_UNDS,
-      KC_LOCK, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,                                      KC_BRID,  KC_BRIU, KC_MUTE, _______, _______, _______,
+      KC_LOCK, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,                                      KC_BRID,  KC_BRIU, _______, _______, _______, _______,
       RGB_TOG, _______, _______, _______, _______, _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______,
-                                 _______, _______, _______, KC_NLCK, KC_SLCK, KC_INS,  _______,  _______, _______, _______
+                                 KC_MUTE, _______, _______, KC_NLCK, KC_SLCK, KC_INS,  _______,  _______, _______, _______
     ),
 // /*
 //  * Layer template

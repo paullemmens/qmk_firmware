e9a835fbc0762811cee327b3e21fc31410d1a1c0
Paul Lemmens <paul.lemmens@gmail.com>
2021-01-19 21:13:19 +0100

    Add personalization from keyria to redox keymap

--- keyboards/redox/keymaps/paullemmens/keymap.c
+++ keyboards/redox/keymaps/paullemmens/keymap.c
@@ -1,5 +1,77 @@
 #include QMK_KEYBOARD_H
 
+#ifdef VELOCIKEY_ENABLE
+#include "velocikey.h"
+#endif
+
+// Tap dance stuff
+#ifdef TAP_DANCE_ENABLE
+enum {
+    TD_5_6,
+    TD_6_7,
+    TD_PERC_CIRC,
+    TD_AMPR_CIRC
+};
+
+qmk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_5_6] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_6),
+    [TD_6_7] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_7),
+    [TD_PERC_CIRC] = ACTION_TAP_DANCE_DOUBLE(KC_PERC, KC_CIRC),
+    [TD_AMPR_CIRC] = ACTION_TAP_DANCE_DOUBLE(KC_AMPR, KC_CIRC)
+};
+#endif
+
+// Personal key definitions:
+// Very special combinations
+#define SGUI_LWR    LM(_LOWER, MOD_LSFT|MOD_LGUI)
+#define GUI_LWR     LM(_LOWER, MOD_LGUI)
+#define O_ACUTE     ALGR(KC_P)
+#define O_UMLAU     ALGR(KC_O)
+#define E_ACUTE     ALGR(KC_E)
+#define E_UMLAU     ALGR(KC_W)
+#define I_UMLAU     ALGR(KC_I)
+#define EURO        ALGR(KC_5)
+#define _ACUTE_     ALGR(KC_QUOTE)
+#define _GRAVE_     ALGR(KC_GRAVE)
+#define _UMLAU_     ALGR(S(KC_QUOTE))
+#define _CIRCFL     ALGR(KC_CIRC)
+// Home row mods
+#define LGUI__Z     LGUI_T(KC_Z)
+#define G_LSHFT     LSFT_T(KC_G)
+#define V_LSHFT     LSFT_T(KC_V)
+#define V_LCTRL     LCTL_T(KC_V)
+#define N_RSHFT     RSFT_T(KC_N)
+#define N_RCTRL     RCTL_T(KC_N)
+#define M_RSHFT     RSFT_T(KC_M)
+#define M_RCTRL     RCTL_T(KC_M)
+#define D_LCTRL     LCTL_T(KC_D)
+#define D_LSHFT     LSFT_T(KC_D)
+#define K_RCTRL     RCTL_T(KC_K)
+#define K_RSHFT     RSFT_T(KC_K)
+#define S__LALT     LALT_T(KC_S)
+#define L__LALT     LALT_T(KC_L)
+#define BS_LALT     LALT_T(KC_BSPC)
+#define ENT_RSF     RSFT_T(KC_ENT)
+// Layer toggles
+#define R_ADJST     LT(_ADJUST, KC_R)
+#define F_RAISE     LT(_RAISE, KC_F)
+#define SPC_LWR     LT(_LOWER, KC_SPC)
+#define DEL_RAI     LT(_RAISE, KC_DEL)
+#define H_LOWER     LT(_LOWER, KC_H)
+#define J_RAISE     LT(_RAISE, KC_J)
+#define U_ADJST     LT(_ADJUST, KC_U)
+
+// https://precondition.github.io/home-row-mods#using-non-basic-keycodes-in-mod-taps
+enum custom_keycodes {
+    MT_OUMLA = SAFE_RANGE,
+    MT_EACUT,
+    MT_RCBR
+};
+#define OU_LCTL LCTL_T(MT_OUMLA)
+#define EA_LSFT LSFT_T(MT_EACUT)
+#define RB_LCTL LCTL_T(MT_RCBR)
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -85,4 +157,66 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   )
+};
+
+#ifdef TAPPING_TERM_PER_KEY
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case K_RSHFT:
+            return TAPPING_TERM + 75;
+        case J_RAISE:
+            return TAPPING_TERM + 25;
+        case N_RCTRL:
+        case M_RCTRL:
+            return TAPPING_TERM + 100;
+        default:
+            return TAPPING_TERM;
+    }
+}
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case OU_LCTL:
+            if (record->tap.count > 0) {
+                if (record->event.pressed) {
+                    // send advanced keycode, etc.
+                    // the 16 bit version of the `tap_code` function is used here
+                    // because KC_HASH is a non-basic keycode.
+                    tap_code16(ALGR(S(KC_QUOTE)));
+                }
+                // do not continue with default tap action
+                // if the MT was pressed or released, but not held
+                return false;
+            }
+            break;
+        case EA_LSFT:
+            if (record->tap.count > 0) {
+                if (record->event.pressed) {
+                    // send advanced keycode, etc.
+                    // the 16 bit version of the `tap_code` function is used here
+                    // because KC_HASH is a non-basic keycode.
+                    tap_code16(ALGR(KC_E));
+                }
+                // do not continue with default tap action
+                // if the MT was pressed or released, but not held
+                return false;
+            }
+            break;
+        case RB_LCTL:
+            if (record->tap.count > 0) {
+                if (record->event.pressed) {
+                    // send advanced keycode, etc.
+                    // the 16 bit version of the `tap_code` function is used here
+                    // because KC_HASH is a non-basic keycode.
+                    tap_code16(KC_RCBR);
+                }
+                // do not continue with default tap action
+                // if the MT was pressed or released, but not held
+                return false;
+            }
+            break;
+    }
+
+    return true;
 };

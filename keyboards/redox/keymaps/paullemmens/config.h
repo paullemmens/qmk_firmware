38218a46436f6802a8efcaec45cfc89cffee1fed
Paul Lemmens <paul.lemmens@gmail.com>
2021-01-19 21:03:11 +0100

    Update config with full RGB animations
    No longer requiring memory use for encoder and oled displays with large
    wpm graphing code.

--- keyboards/redox/keymaps/paullemmens/config.h
+++ keyboards/redox/keymaps/paullemmens/config.h
@@ -26,9 +26,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 14
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_ANIMATIONS
+    /* #define RGBLIGHT_EFFECT_STATIC_LIGHT */
+    /* #define RGBLIGHT_EFFECT_BREATHING */
+    /* #define RGBLIGHT_EFFECT_RAINBOW_MOOD */
+    /* #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
+    #undef RGBLED_NUM
+    #define RGBLED_NUM 14
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 8
+    #define RGBLIGHT_VAL_STEP 8
+    #define RGBLIGHT_SLEEP
+    #define RGBLIGHT_SPLIT
+#endif
+
